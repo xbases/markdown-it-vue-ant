01d787a0901d18079168c18c6ede26fbb44a182e
Xiangyu Zhou <admin@xbases.org                                                                                                                                                                      name = admin>
2019-09-23 17:28:10 +0100

    init

---
+++ src/index.js
@@ -0,0 +1,11 @@
+import MarkdownItVueAnt from "./markdown-it-vue-ant";
+const install = function(Vue) {
+  Vue.component(MarkdownItVueAnt.name, MarkdownItVueAnt);
+};
+
+if (typeof window !== "undefined" && window.Vue) {
+  install(window.Vue);
+}
+
+MarkdownItVueAnt.install = install;
+export default MarkdownItVueAnt;
---
+++ src/markdown-it-vue-ant.vue
@@ -0,0 +1,11 @@
+<template>
+  <div class="markdown-it-vue-ant" ref="markdown-it-vue-ant-container"></div>
+</template>
+<script>
+export default {
+  name: "MarkdownItVueAnt",
+  data() {
+    return {};
+  },
+};
+</script>
